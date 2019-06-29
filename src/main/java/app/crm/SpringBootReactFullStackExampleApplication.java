42c1c1087c3676619c80ff997b3a3e45b4b243c6
ankurgoel92 <ankurgoel.juit@gmail.com>
2019-06-29 15:25:20 +0530

    Initial commit

---
+++ src/main/java/app/crm/SpringBootReactFullStackExampleApplication.java
@@ -0,0 +1,13 @@
+package app.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactFullStackExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactFullStackExampleApplication.class, args);
+	}
+
+}
